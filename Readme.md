bf26b0d548743bb1f16d76c07d8ec75e5f97442b
Utsav <utsavpokemon9000chatterjee@gmail.com>
2022-10-16 22:12:47 +0530

    ready to build

--- Readme.md
+++ Readme.md
@@ -4,7 +4,7 @@
 
 ## USE
 
-    from os_shedulingAlgo_utsav import BestShedulingAlgo
+    from osalgopackage_utsav import BestShedulingAlgo
 
     instance=BestShedulingAlgo(pass the size of set of processes)
     instance.RunAndCompare()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
-    name="os_shedulingAlgo_utsav",
-    version="1.0.0",
+    name="osalgopackage_utsav",
+    version="1.1.0",
     description="Get an ides how OS Sheduling Algorithms Work on a set of Process",
     author="Utsav",
     packages=['osalgopackage_utsav'],
