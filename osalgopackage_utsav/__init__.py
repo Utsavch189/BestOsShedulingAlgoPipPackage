61b6d7720c50fa0786a9982f1aa752e6fdce4426
Utsav <utsavchatterjee71@gmail.com>
2022-10-16 18:24:58 +0530

    first commit

--- osalgopackage_utsav/__init__.py
+++ osalgopackage_utsav/__init__.py
@@ -1,37 +1,36 @@
 
 class BestShedulingAlgo:
 
+	processes=[]
+
+	constant_dicts={
+		"time_quantum":3,
+		"waiting_time":0,
+		"reminder":0
+		}
 
 	def __init__(self,size):
 		self.size=size
-		self.processes=[None]*size
 		print(f"{size} length size is initialized")
 
-	def Merge(dict1,dict2):
-		return {**dict1,**dict2}
-
 	def taking_input(self):
-		constant_dict={
-			"time_quantum":3,
-			"waiting_time":0,
-			"reminder":0
-		}
+
 		print('Enter input in this manner --> processName AT BT P')
 
 		for i in range(0,self.size):
-			name=input('Enter process name')
-			at=input('Enter AT(Arrival Time)')
-			bt=input('Enter BT(Burst Time)')
-			p=input('Enter P(Priority)')
+			name=input('Enter process name: ')
+			at=input('Enter AT(Arrival Time): ')
+			bt=input('Enter BT(Burst Time): ')
+			p=input('Enter P(Priority): ')
 			inpt={
 				"name":name,
 				"AT":int(at),
 				"BT":int(bt),
 				"P":int(p)
 			}
-			self.processes.append(self.Merge(inpt,constant_dict))
-		return lists	
+			self.processes.append({**inpt,**self.constant_dicts})
+		return self.processes	
 
 
 a=BestShedulingAlgo(2)
-a.taking_input()
+print(a.taking_input())
